de9d2ced40aca6a77b9fd5c75f58f67e2b8fc910
Nikita <baraban95@tut.by>
2023-03-13 12:45:09 +0300

    add tabs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-/* $(document).ready(function(){
-    $('.carousel__inner').slick({
+$(document).ready(function(){
+    /*$('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
         prevArrow:'<button type="button" class="slick-prev"><img src="../icons/left.jpg"></button>',
@@ -32,3 +32,23 @@
   document.querySelector('.slick-next').onclick = function () {
     slider.goTo('next');
   };
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+
+  function toggleSlide(item) {
+    $(item).each(function(i) {
+      $(this).on('click', function(e) {
+        e.preventDefault();
+        $('.catalog__item__content').eq(i).toggleClass('catalog__item__content_active');
+        $('.catalog__item__list').eq(i).toggleClass('catalog__item__list_active');
+      })
+    })
+  }
+  toggleSlide('.catalog__link');
+  toggleSlide('.catalog__item__back');
+})
